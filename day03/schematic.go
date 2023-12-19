9820dd03c14abf182e183d32ace81ad9573e0c0d
Bernhard <bernhard@local>
2023-12-19 20:26:45 +0100

    feat: parse nums and symbols into schematic of rows

--- day03/schematic.go
+++ day03/schematic.go
@@ -8,44 +8,68 @@ import (
 	"unicode"
 )
 
-type Row = struct {
-	PartNumbers []PartNumber
-}
+type PartRow []PartNumber
 
-type PartNumber = struct {
+type SymbolRow []PartSymbol
+
+type PartNumber struct {
 	value      int
 	startIndex int
 	endIndex   int
 }
 
-type PartSymbol = struct {
+type PartSymbol struct {
 	value      string
 	startIndex int
 }
 
-func ParseFileToPartRows(filepath string) []PartNumber {
+type Schematic struct {
+	PartRows   []PartRow
+	SymbolRows []SymbolRow
+}
+
+func (schematic *Schematic) addPartRow(partRow PartRow) {
+	schematic.PartRows = append(schematic.PartRows, partRow)
+}
+
+func (schematic *Schematic) addSymbolRow(symbolRow SymbolRow) {
+	schematic.SymbolRows = append(schematic.SymbolRows, symbolRow)
+}
+
+func ParseFileToSchematic(filepath string) Schematic {
 	err, rows := stuff.ScanFile(filepath)
+	schematic := Schematic{}
 
 	if err != nil {
 		log.Fatalf("Error reading file %s\n", filepath)
 	}
 
-	var partNums []PartNumber
 	for _, row := range rows {
-		start := -1
+		partNums := []PartNumber{}
+		partSyms := []PartSymbol{}
+		numStart := -1
 		numstrings := []string{}
 		for index, char := range row {
 			if unicode.IsNumber(char) {
-				start = index
+				if numStart < 0 {
+					numStart = index
+				}
 				numstrings = append(numstrings, string(char))
 			} else {
+				if string(char) != "." {
+					partSyms = append(partSyms, PartSymbol{string(char), index})
+				}
 				if len(numstrings) > 0 {
 					stringNum, _ := strconv.Atoi(strings.Join(numstrings[:], ""))
-					partNums = append(partNums, PartNumber{stringNum, start, start + len(numstrings) - 1})
+					partNums = append(partNums, PartNumber{stringNum, numStart, numStart + len(numstrings) - 1})
 					numstrings = nil
+					numStart = -1
 				}
 			}
 		}
+
+		schematic.addPartRow(partNums)
+		schematic.addSymbolRow(partSyms)
 	}
-	return partNums
+	return schematic
 }
--- day03/schematic_test.go
+++ day03/schematic_test.go
@@ -8,22 +8,37 @@ import (
 var schematicSum = 4361
 var partNums = []int{467, 35, 633, 617, 592, 755, 664, 598}
 var notPartNums = []int{114, 58}
-var expectedPartNums = []PartNumber{
-	{467, 0, 2},
-	{114, 5, 7},
-	{35, 2, 3},
-	{633, 6, 8},
-	{617, 0, 2},
-	{58, 7, 8},
-	{592, 2, 4},
-	{755, 6, 8},
-	{664, 1, 3},
-	{598, 5, 7},
+var expectedRows = []PartRow{
+	{{467, 0, 2}, {114, 5, 7}},
+	{},
+	{{35, 2, 3}, {633, 6, 8}},
+	{},
+	{{617, 0, 2}},
+	{{58, 7, 8}},
+	{{592, 2, 4}},
+	{{755, 6, 8}},
+	{},
+	{{664, 1, 3}, {598, 5, 7}},
+}
+var expectedSymbols = []SymbolRow{
+	{},
+	{{"*", 3}},
+	{},
+	{{"#", 6}},
+	{{"*", 3}},
+	{{"+", 5}},
+	{},
+	{},
+	{{"$", 3}, {"*", 5}},
+	{},
 }
 
-func TestParseFileToPartRows(t *testing.T) {
-	partNums := ParseFileToPartRows("../../AoC2023_data/day03/testdata.txt")
-	if !reflect.DeepEqual(partNums, expectedPartNums) {
-		t.Fatalf("drror:\nWanted: %v\nGot: %v\n", expectedPartNums, partNums)
+func TestParseFileToSchematic(t *testing.T) {
+	schematic := ParseFileToSchematic("../../AoC2023_data/day03/testdata.txt")
+	if !reflect.DeepEqual(schematic.PartRows, expectedRows) {
+		t.Fatalf("Error:\nWanted:\t%v\nGot:\t%v\n", expectedRows, schematic.PartRows)
+	}
+	if !reflect.DeepEqual(schematic.SymbolRows, expectedSymbols) {
+		t.Fatalf("Error:\nWanted:\t%v\nGot:\t%v\n", expectedSymbols, schematic.SymbolRows)
 	}
 }
